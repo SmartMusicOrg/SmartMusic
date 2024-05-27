4189bd1278a90063621a566f8e004fa4a25510a4
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-05-27 18:03:08 +0300

    Upgrading Ui of Buttons page

--- app/src/main/java/com/example/smartmusicfirst/ui/views/ButtonEmotionsScreen.kt
+++ app/src/main/java/com/example/smartmusicfirst/ui/views/ButtonEmotionsScreen.kt
@@ -1,27 +1,35 @@
 package com.example.smartmusicfirst.ui.views
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.smartmusicfirst.TAG
 import com.example.smartmusicfirst.accessToken
 import com.example.smartmusicfirst.connectors.spotify.SpotifyWebApi
 import com.example.smartmusicfirst.playPlaylist
+import com.example.smartmusicfirst.ui.theme.SmartMusicFirstTheme
 
 
 @Composable
 fun SimpleButton(text: String, color: Color, modifier: Modifier = Modifier) {
-    Button(
+    OutlinedButton(
         onClick = {
             SpotifyWebApi.searchForPlaylist("$text mood", accessToken) { playlistId ->
                 if (playlistId.isNotEmpty()) {
@@ -31,25 +39,49 @@ fun SimpleButton(text: String, color: Color, modifier: Modifier = Modifier) {
                 }
             }
         },
+        border = ButtonDefaults.outlinedButtonBorder.copy(width = 3.dp),
+        elevation = ButtonDefaults.elevatedButtonElevation(4.dp),
         colors = ButtonDefaults.buttonColors(containerColor = color),
         modifier = modifier.width(300.dp)
     ) {
         Text(
             text = text,
             style = MaterialTheme.typography.labelLarge,
+            color = MaterialTheme.colorScheme.outline,
             modifier = Modifier.padding(1.dp)
         )
     }
+    Spacer(modifier = Modifier.height(8.dp))
 }
 
 @Composable
 fun MySimpleAppContainer(modifier: Modifier = Modifier) {
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = modifier.background(color = MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         SimpleButton("Happy", color = Color.Green)
         SimpleButton("Excited", color = Color(0xFFFFD151))
         SimpleButton("Angry", color = Color.Red)
         SimpleButton("Optimistic", color = Color.Blue)
-        SimpleButton("Sad", color = Color.Gray)
+        SimpleButton("Sad", color = Color.DarkGray)
         SimpleButton("Energized", color = Color.Cyan)
+    }
+}
+
+@Preview
+@Composable
+fun ButtonEmotionsScreenPreview() {
+    SmartMusicFirstTheme {
+        MySimpleAppContainer(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Preview
+@Composable
+fun ButtonEmotionsScreenDarkPreview() {
+    SmartMusicFirstTheme(darkTheme = true) {
+        MySimpleAppContainer(modifier = Modifier.fillMaxSize())
     }
 }
